7d2b40256597787ad141a62e32a37b17b7af8b8c
Samiur Rahman Sami <49389786+srs404@users.noreply.github.com>
2019-08-29 00:41:49 +0600

    Create WPR by LeyLance.bat

---
+++ WPR by LeyLance.bat
@@ -0,0 +1,26 @@
+@echo off
+title Wifi Password Retriever
+mode 80, 40
+color 0B
+
+
+:start
+cls
+echo    =====================================
+echo.
+echo          Wifi Profile Names
+echo                  LeyLance Solutions
+echo.
+echo    =====================================
+netsh wlan show profiles
+echo.
+set /p a= Wifi Name # 
+if "%a%" == "" goto start
+goto proc
+
+
+:proc
+cls
+netsh wlan show profiles %a% key=clear
+pause>nul
+goto start
